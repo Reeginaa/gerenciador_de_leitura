cab41b9e4dafb2418b3058426b6a8019444916e1
Maria Regina Cerbarp <49926225+Reeginaa@users.noreply.github.com>
2019-06-19 18:10:11 -0300

    Commit 19/06
    Página inicial corrigida e estilizada

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Página Inicial</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="menu">
+			<ul>
+				<li><a href="src/indexUsuario.php">Manutenção de Usuário</a></li>|
+				<li><a href="src/indexLivro.php">Manutenção de Livro</a></li>|
+				<li><a href="src/indexMeta.php">Manutenção de Meta</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
